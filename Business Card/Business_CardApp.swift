58c44812af09215c61df342c9f13925585b1ab8b
Aavhan Chatse <chatseaavhan@gmail.com>
2024-09-30 16:36:00 +0530

    Initial Commit

---
+++ Business Card/Business_CardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Business_CardApp.swift
+//  Business Card
+//
+//  Created by Aavhan Chatse on 30/09/24.
+//
+
+import SwiftUI
+
+@main
+struct Business_CardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
